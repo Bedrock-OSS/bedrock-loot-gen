b663f3d9a852b9fd36eb240773e0dce26bcb8bdb
SirLich <lrkoehler77@gmail.com>
2020-07-06 17:32:19 -0500

    Fix port

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ app.get('/loot_table.json',function(req,res) {
   res.sendFile(__dirname + '/schemas/loot_table.json');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
 });
 
 app.get('/style.css',function(req,res) {
